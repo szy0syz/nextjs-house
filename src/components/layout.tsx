6efc3b3a05763b126f27ef81a20d25b554906951
szy0syz <szy0syz@gmail.com>
2022-02-07 11:29:07 +0800

    update: 完成Layout组件

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,42 @@
-// import { FunctionComponent, ReactNode } from "react";
-// import Link from "next/link";
+import { FC, ReactNode } from "react";
+import Link from "next/link";
+import link from "next/link";
 // import { useAuth } from "src/auth/useAuth";
 
-export {};
+interface IProps {
+  main: ReactNode;
+}
+
+const Layout: FC<IProps> = ({ main }) => {
+  const authenticated = false;
+  const logout = () => null;
+
+  return (
+    <div className="mx-auto text-white bg-gray-900 max-w-screen-2xl">
+      <nav className="bg-gray-800" style={{ height: "64px" }}>
+        <div className="flex items-center justify-between h-16 px-6">
+          <Link href="/">
+            <a>
+              <img src="/home-color.svg" alt="home" className="inline w-6" />
+            </a>
+          </Link>
+          {authenticated ? (
+            <>
+              <Link href="/houses/add">
+                <a>Add House</a>
+              </Link>
+              <button onClick={() => {}}>Logout</button>
+            </>
+          ) : (
+            <Link href="/auth">
+              <a>Login / Signup</a>
+            </Link>
+          )}
+        </div>
+      </nav>
+      <main style={{ minHeight: "calc(100vh - 64px)" }}>{main}</main>
+    </div>
+  );
+};
+
+export default Layout;
