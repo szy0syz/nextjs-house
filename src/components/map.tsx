03e86597099e580df4747fd39d005197fd55b263
szy0syz <szy0syz@gmail.com>
2022-02-07 15:30:50 +0800

    update: 完成MapBox组件

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,10 +1,35 @@
-// import { useRef, useState } from "react";
-// import Link from "next/link";
-// import { Image } from "cloudinary-react";
-// import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
+import { useRef, useState } from "react";
+import Link from "next/link";
+import { Image } from "cloudinary-react";
+import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 // import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery_houses } from "src/generated/HousesQuery";
 // import { SearchBox } from "./searchBox";
 
-export {};
+interface IProps {}
+
+export default function Map({}: IProps) {
+  const mapRef = useRef<ReactMapGL | null>(null);
+  const [viewport, setViewport] = useState<ViewState>({
+    latitude: 25.05076044062315,
+    longitude: 102.70467539584287,
+    zoom: 11,
+  });
+
+  return (
+    <div className="relative text-black">
+      <ReactMapGL
+        {...viewport}
+        width="100%"
+        height="calc(100vh - 64px)"
+        onViewportChange={setViewport}
+        ref={(instance) => (mapRef.current = instance)}
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+        minZoom={8}
+        maxZoom={15}
+        mapStyle="mapbox://styles/leighhalliday/ckhjaksxg0x2v19s1ovps41ef"
+      ></ReactMapGL>
+    </div>
+  );
+}
